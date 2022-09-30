b8bed7dbabc1e4cf66ea974d79a0a5ac22528e3e
dev-parthive <dev-parthive>
2022-09-30 21:48:27 +0600

    some minor change in json data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
-// import { ToastContainer, toast } from 'react-toastify';
 function App() {
   return (
     <div className="App">
