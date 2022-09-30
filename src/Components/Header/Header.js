163bbd7c0d8d017101cb58ae2bc6d99634c29e96
dev-parthive <dev-parthive>
2022-09-30 20:47:10 +0600

    Header component added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../Images/dumbbell.png'
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+           <nav className='nav-section'>
+            <img className='logo-images' src={logo} alt="" />
+            <h1 className='heading-line'>BD Challengers Club</h1>
+           </nav>
+        </div>
+    );
+};
+
+export default Header;
